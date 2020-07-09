2c94f1e0d9d766fb3016883dba9c812c4d78c2cd
Gaurav Mendse <gauravmendse@Gauravs-MacBook-Pro.local>
2020-07-09 18:01:58 +0530

    stored user in app as well as in the firestore database document

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Authentication from './pages/authentication/authentication.component';
 import Header from './components/header/header.component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
   constructor() {
@@ -20,6 +20,8 @@ class App extends React.Component {
 
   unsubscribeFromAuth = null;
 
+  resetUserState = () => this.setState({ currentUser: null });
+
   componentDidMount() {
     // onAuthStateChanged is a open subscription (open message system) between the app and firebase
     // that is, whenever any changes occur on firebase, firebase sends out a message that user has updated
@@ -27,8 +29,29 @@ class App extends React.Component {
     // since this is an open subscription, we also want to close the subscription since we don't want any memory leaks from JavaScript
     // onAuthStateChanged returns a function inside the unsubscribeFromAuth property, which when called when component unmounts
     // would closes the subscription
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        try {
+          const userRef = await createUserProfileDocument(userAuth);
+
+          if (!userRef) throw Error('Error getting user documentRef');
+
+          // Get the data from document ref object using snapshot.data() method
+          userRef.onSnapshot(snapshot => {
+            this.setState({
+              currentUser: {
+                id: snapshot.id,
+                ...snapshot.data()
+              }
+            });
+          });
+        } catch (error) {
+          console.log('Error getting user login details', error.message);
+          this.resetUserState();
+        }
+      } else {
+        this.resetUserState();
+      }
     });
   }
 
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -17,6 +17,31 @@ const config = {
   measurementId: 'G-FPQX54RF02'
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = await firestore.doc(`users/${userAuth.uid}`);
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log('Error creating user', error.message);
+    }
+  }
+
+  return userRef;
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();

61774e06e6459c0b1028ef8e2c613de3057fd56b
Gaurav Mendse <gauravmendse@Gauravs-MacBook-Pro.local>
2020-07-14 14:07:29 +0530

    added redux-persist for store to have persistence capability

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,23 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
 
-export default combineReducers({
+/** Pass in the reducer we want to persist, i.e. the cart reducer in this case
+ * User reducer persistance is already handled by firebase
+ */
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart']
+};
+
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer
 });
+
+/** Use persistReducer HOC to return modified version of rootReducer with persistence capabilities */
+export default persistReducer(persistConfig, rootReducer);

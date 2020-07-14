480879f5c1ee790da8fffdd88f4da7681ed4442a
Gaurav Mendse <gauravmendse@Gauravs-MacBook-Pro.local>
2020-07-14 12:41:37 +0530

    added addItem and removeItem functionality to checkout item component

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,20 +1,44 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
+import {
+  clearCartItem,
+  addCartItem,
+  removeCartItem
+} from '../../redux/cart/cart.actions';
 
 import './checkout-item.styles.scss';
 
-const CheckoutItem = ({ cartItem: { name, imageUrl, price, quantity } }) => {
+const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
+  const { name, imageUrl, price, quantity } = cartItem;
   return (
     <div className='checkout-item'>
       <div className='image-container'>
         <img src={imageUrl} alt='item' />
       </div>
       <span className='name'>{name}</span>
-      <span className='quantity'>{quantity}</span>
+      <span className='quantity'>
+        <div className='arrow' onClick={() => removeItem(cartItem)}>
+          &#10094;
+        </div>
+        <span className='value'>{quantity}</span>
+        <div className='arrow' onClick={() => addItem(cartItem)}>
+          &#10095;
+        </div>
+      </span>
       <span className='price'>${price}</span>
       {/* Refer UTF-8 Wingdings at w3schools for the x synbol used */}
-      <div className='remove-button'>&#10005;</div>
+      <div className='remove-button' onClick={() => clearItem(cartItem)}>
+        &#10005;
+      </div>
     </div>
   );
 };
 
-export default CheckoutItem;
+const mapDispatchToProps = dispatch => ({
+  clearItem: item => dispatch(clearCartItem(item)),
+  addItem: item => dispatch(addCartItem(item)),
+  removeItem: item => dispatch(removeCartItem(item))
+});
+
+export default connect(null, mapDispatchToProps)(CheckoutItem);
--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -2,7 +2,17 @@ import { CartActionTypes as CAT } from './cart.types';
 
 export const toggleCartHidden = () => ({ type: CAT.TOGGLE_CART_HIDDEN });
 
-export const addCartItem = newCartItem => ({
+export const addCartItem = item => ({
   type: CAT.ADD_CART_ITEM,
-  payload: newCartItem
+  payload: item
+});
+
+export const removeCartItem = item => ({
+  type: CAT.REMOVE_CART_ITEM,
+  payload: item
+});
+
+export const clearCartItem = item => ({
+  type: CAT.CLEAR_CART_ITEM,
+  payload: item
 });
